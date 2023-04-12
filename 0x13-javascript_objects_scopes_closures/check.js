c5017779e6be0be62e1055eef4b2fae5abaa145b
abbeylawal <lawalabbeysmith@gmail.com>
2023-04-12 15:28:51 +0100

    task objects.js

---
+++ 0x13-javascript_objects_scopes_closures/check.js
@@ -0,0 +1,17 @@
+/* let b = 1;
+
+function myFunction(a) {
+    console.log(a + b);
+    b = a;
+}
+
+myFunction(3);
+myFunction(4); */
+
+const a = 12;
+
+function myFunction(a) {
+    console.log(a);
+}
+
+myFunction(89);
