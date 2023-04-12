8b4d1eabb6aca2ccbae2aedeef06a193469c49dd
abbeylawal <lawalabbeysmith@gmail.com>
2023-04-12 15:49:39 +0100

    task 9-add.js update chmod

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,14 @@
 #!/usr/bin/node
-function add (a, b) {
-    const c = a + b;
-    console.log(c);
+function add(a, b) {
+  const sum = a + b;
+  console.log(sum);
 }
 
-add(Number(process.argv[2]), Number(process.argv[3]));
+const num1 = Number(process.argv[2]);
+const num2 = Number(process.argv[3]);
+
+if (isNaN(num1) || isNaN(num2)) {
+  console.log("Please provide two valid integers as command line arguments");
+} else {
+  add(num1, num2);
+}
